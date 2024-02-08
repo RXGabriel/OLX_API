54e7f96c6ff655bfadce30c6300ddb1e0e9b5c1c
RXGabriel <gabriel2005rf@gmail.com>
2024-02-08 10:49:51 -0300

    Creating routes

---
+++ src/model/State.js
@@ -0,0 +1,14 @@
+import mongoose, { Promise } from "mongoose";
+Promise = global.Promise;
+
+const userSchema = new mongoose.Schema({
+  name: String,
+});
+
+const modelName = "State";
+
+if (mongoose.connection && mongoose.connection.models[modelName]) {
+  module.exports = mongoose.connection.models[modelName];
+} else {
+  module.exports = mongoose.model(modelName, userSchema);
+}
